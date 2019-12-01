423f4ee0497b0782b2b226ae3ae8eb760d634c8d
EdwardEisenhauer <sergiusz.warga@gmail.com>
2019-12-01 23:09:23 +0100

    Divided Player into subclasses

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from game import Game
-from player import Player
+from player import Player, Human, Heuristic, QAgent
 from enums import Mode, Field
 
-games_to_play = 3
-tic_tac_toe = Game((Player(Field.X, mode=Mode.HUMAN), Player(Field.O, mode=Mode.Q, gamma=0.1, epsilon=0.3)),
+import sys
+
+games_to_play = int(sys.argv[1])
+human_player = Human(Field.X)
+random_player = Player(Field.X)
+heuristic_player = Heuristic(Field.X)
+q_player = QAgent(Field.O, alpha=1.0, gamma=0.1, epsilon=0.1)
+tic_tac_toe = Game((random_player, q_player),
                    board_size=3, draw=True)
 for episode in range(games_to_play):
     tic_tac_toe.play()
     tic_tac_toe.reset()
 tic_tac_toe.visualize_stats()
+tic_tac_toe.print_stats()
